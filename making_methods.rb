610117735e3b7cf5bd5db987a7a90a2bf72503c6
Aaron Gooden <aagooden@me.com>
2018-03-12 17:56:08 -0400

    Tests for upcase and insert written and passing

--- making_methods.rb
+++ making_methods.rb
@@ -10,3 +10,38 @@ end
 
 reversed = reverse_string("cheese")
 puts reversed
+
+
+
+def upcase(stuff)
+
+	new_string = ""
+	x = 0
+	stuff.each_char do |i|
+		new_string[x] = i.capitalize
+		x+=1
+	end
+	return new_string
+
+end
+
+def insert(stuff, index, other)
+
+	new_string = ""
+	x = 0
+	mod = 0
+	stuff.each_char do |i|
+		if x == index
+			new_string[x] = other
+			mod = 1
+		end
+		
+		new_string[x+mod] = i
+		puts new_string
+	
+		x+=1
+	end
+	return new_string
+
+end
+
--- test_making_methods.rb
+++ test_making_methods.rb
@@ -15,4 +15,33 @@ class Making_methods_test < Minitest::Test
 		assert_equal("eseehc", reverse_string("cheese"))
 	end
 
+	def test_upcase_returns_string
+		assert_equal(String, upcase("cheese").class)
+	end
+
+	def test_upcase_returns_capital_a
+		assert_equal("A", upcase("a"))
+	end
+
+	def test_upcase_returns_capital_cheese
+		assert_equal("CHEESE", upcase("cheese"))
+	end
+
+	def test_upcase_returns_capital_MaRsHAlL
+		assert_equal("MARSHALL", upcase("MaRsHAlL"))
+	end	
+
+	def test_insert_returns_string
+		assert_equal(String, insert("cheese", 2, "i").class)
+	end	
+
+	def test_insert_returns_chzeese
+		assert_equal("chzeese", insert("cheese", 2, "z"))
+	end		
+
+	def test_insert_returns_AbCdQef
+		assert_equal("AbCQdef", insert("AbCdef", 3, "Q"))
+	end	
+
+
 end
